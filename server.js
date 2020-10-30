03f9e347d4838ce9c8b825a9b23b81f081a54a44
Kishan movaliya <km.globaliasoft@gmail.com>
2020-10-30 18:28:20 +0530

    rename the files

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ emailService.sendMail()
 emailsnoozeService.sendMailsnooze()
 
 //----------------User route for login signup---------------------------------
-const userRoutes = require('./routes/user.route')
+const userRoutes = require('./routes/userroute')
 
 //----------------use the packages--------------------------------------------
 const app = express();
